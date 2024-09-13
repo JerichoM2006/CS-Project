6db120077b4ef0419ad6d724381eef8ab8a99346
NotAHuman1230 <jerichojohn@outlook.com>
2024-09-13 19:38:18 +0100

    EncryptionSystem completion

--- EncryptionSystem.py
+++ EncryptionSystem.py
@@ -1,13 +1,43 @@
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Cipher import AES
+import os
 
 class EncryptionSystem:
-    def __init__(self, password, salt):
-        #derive aes key from password and salt
-        self.key = PBKDF2(password, salt, dkLen=32, count=10000, prf=None)
+    def __init__(self, password):
+        self.password = password
 
-    def encrypt(self):
-        pass
+    def encrypt(self, path):
+        salt = os.urandom(16)
+        key = PBKDF2(self.password, salt, dkLen=32, count=10000, prf=None)
 
-    def decrypt(self):
-        pass
+        cipher = AES.new(key, AES.MODE_GCM)
+        nonce = cipher.nonce
+
+        with open(path, 'rb') as f:
+            plaintext = f.read()
+
+        ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+        with open(path, 'wb') as f:
+            f.write(salt + nonce + tag + ciphertext)
+
+        print("Encryption successful")
+
+    def decrypt(self, path):
+        with open(path, 'rb') as f:
+            salt = f.read(16)
+            nonce = f.read(16)
+            tag = f.read(16)
+            ciphertext = f.read()
+
+        key = PBKDF2(self.password, salt, dkLen=32, count=10000, prf=None)
+
+        cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+        try:
+            plaintext = cipher.decrypt_and_verify(ciphertext, tag)
+
+            with open(path, 'wb') as f:
+                f.write(plaintext)
+        except ValueError:
+            print("Decryption failed")
+
+        print("Decryption successful")
