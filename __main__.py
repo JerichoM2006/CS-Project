a1083ba634f2e2e12d7bc8d356f7f40b182e3b41
NotAHuman1230 <jerichojohn@outlook.com>
2024-09-13 19:50:42 +0100

    PrimitiveTranscription testing

--- __main__.py
+++ __main__.py
@@ -1,36 +1,22 @@
 import DesktopRecording
 import Threadpool
-import wave
-import os
+import PrimitiveTranscription
 
 def main():
     pool = Threadpool.Threadpool(10)
     recording = DesktopRecording.desktopRecording(pool)
+    transcriptionAI = PrimitiveTranscription.Transcription()
     recording.startRecording()
 
     while True:
-        inp = input("1. Get from buffer\n2. Get buffer length\n3. Stop recording\n")
-
-        if inp == "1":
-            print("Segment fetched")
-
-            name = "segment"
-            index = 1
-            while os.path.exists(name + str(index) + ".wav"):
-                index += 1
-            name =name + str(index) + ".wav"
-
-            waveFile = wave.open(name, 'wb')
-            waveFile.setnchannels(recording.channels)
-            waveFile.setsampwidth(recording.p.get_sample_size(recording.format))
-            waveFile.setframerate(recording.rate)
-            waveFile.writeframes(recording.getSegment())
-            waveFile.close()
-        elif inp == "2":
-            print("Buffer length: ", recording.buffer.qsize())
-        elif inp == "3":
-            recording.stopRecording()
-            break
+        if recording.buffer.qsize() > 0:
+            transcript = transcriptionAI.createTranscript(recording.channels, 
+                                                    recording.p.get_sample_size(recording.format), 
+                                                    recording.rate, 
+                                                    recording.getSegment())
+            
+            print(transcript, end=" ")
+            
 
 if __name__ == "__main__":
     main()
