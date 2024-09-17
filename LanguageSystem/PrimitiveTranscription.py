935043b80193ff777462d3aeecfa5d09fae039bf
NotAHuman1230 <jerichojohn@outlook.com>
2024-09-17 11:57:34 +0100

    How did this get here?

--- LanguageSystem/PrimitiveTranscription.py
+++ LanguageSystem/PrimitiveTranscription.py
@@ -33,20 +33,20 @@ class Transcription:
         
     def generateTranscript(self):
         while not self.stopTranscript.is_set():
-            if self.recording.buffer.qsize() > 0:
-                segmentFile = io.BytesIO()
-                with wave.open(segmentFile, 'wb') as f:
-                    f.setnchannels(self.recording.channels)
-                    f.setsampwidth(self.recording.p.get_sample_size(self.recording.format))
-                    f.setframerate(self.recording.rate)
-                    f.writeframes(self.recording.getSegment())
-                segmentFile.seek(0)
+            segmentFile = io.BytesIO()
+            record = self.recording.getSegment()
+            with wave.open(segmentFile, 'wb') as f:
+                f.setnchannels(self.recording.channels)
+                f.setsampwidth(self.recording.p.get_sample_size(self.recording.format))
+                f.setframerate(self.recording.rate)
+                f.writeframes(record)
+            segmentFile.seek(0)
 
-                with sr.AudioFile(segmentFile) as source:
-                    self.r.adjust_for_ambient_noise(source)
-                    audio = self.r.record(source)
-                
-                try:
-                    self.transcriptBuffer.put(self.r.recognize_google(audio, language=self.language))
-                except sr.UnknownValueError:
-                    self.transcriptBuffer.put("...")
+            with sr.AudioFile(segmentFile) as source:
+                self.r.adjust_for_ambient_noise(source)
+                audio = self.r.record(source)
+            
+            try:
+                self.transcriptBuffer.put(self.r.recognize_google(audio, language=self.language))
+            except sr.UnknownValueError:
+                self.transcriptBuffer.put("...")
